3cad6ec921004a2ae6c9f735f7965283552a3248
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2018-10-08 20:35:08 +0530

    Codes Added

--- Water_Level_Identifier_V1.1/src/my_waterlevel/Splitter.java
+++ Water_Level_Identifier_V1.1/src/my_waterlevel/Splitter.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package my_waterlevel;
 
 /**
  *
  * @author Sithum Ravishara
  */
-public class Splitter {
-    
+public class Splitter implements Observer{
+     @Override    
+     public void update(int waterLevel) {        
+         System.out.println(waterLevel>=75 ? "Splitter ON" :"Splitter OFF");     
+     }
 }
